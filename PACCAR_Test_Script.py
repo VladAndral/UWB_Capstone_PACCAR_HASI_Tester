0bd7efce8f0fa9c67ed85375ec8c95e89a930cf5
kgarci5-coder <kgarci5@uw.edu>
2026-02-23 17:35:27 -0800

    py_canoe example

--- PACCAR_Test_Script.py
+++ PACCAR_Test_Script.py
@@ -1,40 +1,22 @@
-#!/usr/bin/env python
+import os
+from py_canoe import CANoe
 
-"""
-This example shows how sending a single message works.
-"""
-
-import can
-
-
-def send_one(): 
-    """Sends a single message."""
-
-    # this uses the default configuration (for example from the config file)
-    # see https://python-can.readthedocs.io/en/stable/configuration.html
-    # BA_ "Network" BO_ 2566540803 "VCAN20:ADSCAN1";
-    with can.Bus() as bus:
-        # Using specific buses works similar:
-        bus = can.Bus(interface='socketcan', channel='vcan20', bitrate=250000)
-      
-     
-
-        msg = can.Message(
-            arbitration_id=0xC0FFEE, 
-            data=[0, 25, 0, 1, 3, 1, 4, 1], 
-            is_extended_id=True
-        )
-
-        try:
-            bus.send(msg)
-            print(f"Message sent on {bus.channel_info}")
-        except can.CanError:
-            print("Message NOT sent")
-
-    with can.Bus() as bus:
-        
-        bus = can.Bus(interface='socketcan', channel='ADSCAN1', bitrate=250000)
-
+def test_connection():
+    print("Initializing CANoe wrapper...")
+    canoe = CANoe()
+    
+    
+    cfg_path = r"C:\Users\Public\Documents\Vector\CANoe\Projects\J1939_CAN_FD_2ch\J1939_CAN_FD_2ch.cfg"
+    
+    print(f"Telling Windows to open/attach to: {cfg_path}")
+    
+    
+    canoe.open(cfg_path)
+    
+    print("Fetching version info...")
+    version_info = canoe.get_canoe_version_info()
+    
+    print(f"Success! Connected to CANoe version: {version_info}")
 
 if __name__ == "__main__":
-    send_one()
+    test_connection()
