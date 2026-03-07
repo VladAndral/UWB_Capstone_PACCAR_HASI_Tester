e29528263fd3366e3ed21de57a668a934f55637e
seanb9800 <seanb9800@gmail.com>
2026-03-06 16:54:42 -0800

    Added decimal Arb ID and gateway comments for checking against .dbc file. New users will need to add/push their file paths.

--- cantest1.py
+++ cantest1.py
@@ -6,16 +6,26 @@ def cantest1():
     
     # SEAN FILE PATH
     cfg_path = r"C:\Users\seanb\Capstone\J1939_CAN_FD_2ch\J1939_CAN_FD_2ch.cfg"
+
+    # KENDALL FILE PATH
+    # cfg_path = r"C:\Users\Public\Documents\Vector\CANoe\Projects\J1939_CAN_FD_2ch\J1939_CAN_FD_2ch.cfg"
     canoe.open(cfg_path)
     
     canoe.start_measurement()
 
-    # canoe.set_system_variable_value("PACCAR_HASI::CAN_ID_TX", arbitrationID)  
+    # canoe.set_system_variable_value("PACCAR_HASI::CAN_ID_TX", arbitrationID)
+    # Arb ID: 2565866791 "VCAN2:VCAN20"  
     canoe.set_system_variable_value("PACCAR_HASI::CAN_ID_TX", 0x18F00527)
     canoe.set_system_variable_value("PACCAR_HASI::Trigger", 1)
 
-    #CAN ID 2
+    # CAN ID 2
+    # Arb ID: 2564487427 "VCAN20:VCAN2"
     canoe.set_system_variable_value("PACCAR_HASI::CAN_ID_TX", 0x18DAF903)
+    canoe.set_system_variable_value("PACCAR_HASI::Trigger", 1)
+
+    # CAN ID 3
+    # Arb ID: 2566541214 "ADSCAN1:VCAN2, ADSCAN1:VCAN20"
+    canoe.set_system_variable_value("PACCAR_HASI::CAN_ID_TX", 0x18FA4F9E)
     canoe.set_system_variable_value("PACCAR_HASI::Trigger", 1)
         
     canoe.stop_measurement()
