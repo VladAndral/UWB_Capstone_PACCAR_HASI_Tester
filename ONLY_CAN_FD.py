713f96034e5a67cde87c129413915c4103b52b41
kgarci5-coder <kgarci5@uw.edu>
2026-04-13 18:44:24 -0700

    added a file purely for testing can fd

--- ONLY_CAN_FD.py
+++ ONLY_CAN_FD.py
@@ -1,29 +1,15 @@
 import utils, sys, can, re
 from can.interfaces import vector
 
+dbc_ID = 2633727015
+print(f"initial arbID: {dbc_ID}")
+formattedID = int(utils.format_arbitrationID(str(dbc_ID), "int"))
+print(f"formatted id: {formattedID}")
+FD_formattedID = utils.convertFD_toCAN(formattedID)
+print(f"result: {hex(FD_formattedID)}")
+
 
 def run__injection_gateway_test():
-    """Test script that loops through all gateway networks, injecting CAN messages
-        into each bus, and listening on one bus to see if the message was sent
-
-    Args:
-        isVirtualInterface (bool): Option to force a virtual interface,
-        even if hardware like Vector or Kvaser are connected
-    """
-    
-    # Get the config info from one of the channels on the Vector hardware
-    config_ch1 = str(can.detect_available_configs(interfaces=['vector'])[0])
-    
-    # Find the exact string 'bitrate=#####', focusing on the number (creating a group)
-    channelBitrate = re.search(r"bitrate=(\d+)", config_ch1 )
-    
-    # Only get the string captured in the group (the number; the bitrate)
-    if channelBitrate: channelBitrate = channelBitrate.group(1)
-    if isinstance(channelBitrate, str):
-        BITRATE_CONST = int(channelBitrate)
-    else:
-        # If what was returned from channelBitrate.group(1) was not a string, don't use it
-        raise TypeError("Error returning string from regex searching for bitrate")
     
     # load the dbc filepath entered in filepath.txt
     primaryDBC_filepath = utils.loadFilePath("primaryDBC")
@@ -31,6 +17,10 @@ def run__injection_gateway_test():
     
     # Dictionary of all gateways. Key=arbitrationID, value=list of channels gatewayed
     gatewaySpecDict = utils.scrape_dbc_for_gateways(primaryDBC_filepath)
+    
+    print(f"Found {len(gatewaySpecDict)} gateway IDs in DBC.")
+    if len(gatewaySpecDict) == 0:
+        return
 
     for arbitrationID_raw, channelList in gatewaySpecDict.items():
         # Formatting arbitrationID and converting to int format for python-can to use
@@ -46,13 +36,13 @@ def run__injection_gateway_test():
             senderName = eachChannel[0]
             receiverName = eachChannel[1]
             # This if branch is only for testing non CAN FD ports ADSCAN#, since they require special handling/formatting
-            if senderName[0] == ('V' or 'P') and receiverName[0] == ('V' or 'P'):
+            if senderName in ('ADSCAN1', 'ADSCAN2') or receiverName in ('ADSCAN1', 'ADSCAN2'):
                 
                 print("\nThis gateway pair: {} to {}. Order matters. (arbID {})".format(senderName, receiverName, arbitrationID_raw))
                 input("Press enter when switched. Test outputs will be affected if the switch is not made.")
             
-                sender = vector.VectorBus(serial=535823, channel=0, bitrate = BITRATE_CONST)
-                receiver = vector.VectorBus(serial=535823, channel=1, bitrate = BITRATE_CONST)
+                sender = vector.VectorBus(serial=535823, channel=0, bitrate = 500000)
+                receiver = vector.VectorBus(serial=535823, channel=1, bitrate = 500000)
                 
                 try:
                     sender.send(msg)
@@ -79,3 +69,4 @@ def run__injection_gateway_test():
 
 if __name__ == "__main__":
     run__injection_gateway_test()
+    
